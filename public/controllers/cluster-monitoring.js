17a26449ad11fa5a18063e1b392c53cad23d9f27
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-01-05 14:43:10 +0100

    Givin methods to cluster monitoring controller

--- public/controllers/cluster-monitoring.js
+++ public/controllers/cluster-monitoring.js
@@ -1,13 +1,82 @@
 let app = require('ui/modules')
 .get('app/wazuh', [])
-.controller('clusterController', function ($scope, clusterMonitoring) {
+.controller('clusterController', function ($scope, clusterMonitoring, Notifier) {
+    const notifier = new Notifier();
+    $scope.clusterTab = 'node-info';
+    $scope.dataShown = null;
 
-    Promise.all([
-        clusterMonitoring.getNodeInfo(), 
-        clusterMonitoring.getAgents(), 
-        clusterMonitoring.getFiles(), 
-        clusterMonitoring.getNodes(), 
-        clusterMonitoring.getStatus(), 
-        clusterMonitoring.getConfig()
-    ]).then(data => console.log(data));
+    const loadNodeInfo = async () => {
+        try {
+            const data = await clusterMonitoring.getNodeInfo();
+        } catch (error) {
+            notify.error(error);
+        }
+    }
+
+    const loadAgentsInfo = async () => {
+        try {
+            const data = await clusterMonitoring.getAgents();
+        } catch (error) {
+            notify.error(error);
+        }
+    }
+
+    const loadFilesInfo = async () => {
+        try {
+            const data = await clusterMonitoring.getFiles();
+        } catch (error) {
+            notify.error(error);
+        }
+    }
+
+    const loadNodesInfo = async () => {
+        try {
+            const data = await clusterMonitoring.getNodes();
+        } catch (error) {
+            notify.error(error);
+        }
+    }
+
+    const loadStatusInfo = async () => {
+        try {
+            const data = await clusterMonitoring.getStatus();
+        } catch (error) {
+            notify.error(error);
+        }
+    }
+
+    const loadConfigInfo = async () => {
+        try {
+            const data = await clusterMonitoring.getConfig();
+        } catch (error) {
+            notify.error(error);
+        }
+    }
+
+    switch($scope.clusterTab){
+        case 'node-info':
+            loadNodeInfo();
+            break;
+        case 'agents-info':
+            loadAgentsInfo();
+            break;
+        case 'files-info':
+            loadFilesInfo();
+            break;
+        case 'nodes-info':
+            loadNodesInfo();
+            break;
+        case 'status-info':
+            loadStatusInfo();
+            break;
+        case 'config-info':
+            loadConfigInfo();
+            break;
+        default:
+            loadNodeInfo();
+            break;
+    };
+
+    loadNodeInfo();
+
 });
