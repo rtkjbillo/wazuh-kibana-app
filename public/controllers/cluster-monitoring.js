f177ac87f793c459ed37b11d8fc32206d5700263
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-01-09 18:02:46 +0100

    Fixes agents and files from current node

--- public/controllers/cluster-monitoring.js
+++ public/controllers/cluster-monitoring.js
@@ -5,14 +5,14 @@ let app = require('ui/modules')
     const notifier      = new Notifier();
     $scope.selectedNode    = null;
     $scope.clusterTab      = 'agents-info';
-    $scope.dataShown       = null;
+    $scope.dataShown       = [];
     $scope.config          = null;
     $scope.dataShownHeader = null;
     $scope.loading         = true;
     $scope.error           = null;
     $scope.raw             = null;
     $scope.lookingNode     = false;
-
+    
     $scope.switchNode = async item => {
         $scope.lookingNode  = true;
         $scope.selectedNode = item;
@@ -25,8 +25,8 @@ let app = require('ui/modules')
 
     $scope.switchClusterTab = tab => {
         $scope.showRaw    = false;
-        $scope.dataShown  = 'Loading...';
-        $scope.raw        = 'Loading...';
+        $scope.dataShown  = [];
+        $scope.raw        = 'Loading';
         $scope.clusterTab = tab;
         switch($scope.clusterTab){
             case 'agents-info':
@@ -63,13 +63,7 @@ let app = require('ui/modules')
             if(data.data.error) {
                 return handleError(data.data.error);
             }
-            let fixed = [];
-            for(let key in data.data.data){
-                for(let agent of data.data.data[key]){
-                    agent.node = key;
-                    fixed.push(agent);
-                }
-            }
+            const fixed = data.data.data[$scope.selectedNode.node] || [];
             data.data.data = fixed;
             return handleData(data);
         } catch (error) {
@@ -80,8 +74,18 @@ let app = require('ui/modules')
     const loadFilesInfo = async () => {
         try {
             $scope.loading = true;
-            const data = await clusterMonitoring.getFiles($scope.selectedNode.node);
-
+            const data = await clusterMonitoring.getFiles();
+            data.data.data = data.data.data[$scope.selectedNode.url];
+            let fixed = [];
+            for(let key in data.data.data){
+                for(let file of data.data.data[key]){
+                    fixed.push({
+                        file: file,
+                        status: key
+                    });
+                }
+            }
+            data.data.data = fixed;
             if(data.data.error) {
                 return handleError(data.data.error);
             }
